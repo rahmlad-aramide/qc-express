14049f38d0f5ee400bd37599fe58f65f292d2a45
rahmlad-aramide <abdrahmanoladimeji02@gmail.com>
2023-11-30 17:12:03 +0100

    chore: added toastify and react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,9 @@
 import Login from "./auth";
+// import Login from "./auth";
+import Signup from "./auth/signup"
+import { ToastContainer, toast } from "react-toastify";
+
+import "react-toastify/dist/ReactToastify.css";
 
 function App() {
   return (
