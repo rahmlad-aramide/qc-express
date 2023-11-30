ff8e1b2e692bc929e8b5dbc7973914be8cc467f4
Frokesy <akindeleayanfeoluwa@gmail.com>
2023-11-30 16:49:35 +0100

    added frontend validation for signup page

--- src/auth/signup.tsx
+++ src/auth/signup.tsx
@@ -1,28 +1,139 @@
+import { useState } from "react";
 import AuthContainer from "../components/containers/AuthContainer";
 import InputField from "../components/default/InputField";
+import { States } from "../components/miscellanous/states";
+
 const Signup = () => {
+  const [user, setUser] = useState({
+    firstname: "",
+    lastname: "",
+    username: "",
+    country: "",
+    email: "",
+    phone: "",
+    city: "",
+    state: "",
+    house: "",
+    password: "",
+    confirmPassword: "",
+  });
+  const [error, setError] = useState({
+    firstname: "",
+    lastname: "",
+    username: "",
+    country: "",
+    email: "",
+    phone: "",
+    city: "",
+    state: "",
+    house: "",
+    password: "",
+    confirmPassword: "",
+  });
+
+  // const [loading, setLoading] = useState(false);
+
+  const validateField = ( value: string) => {
+    if (value === "") {
+      return false;
+    } else {
+      return true;
+    }
+  };
+
+  const validatePassword = (password: string) => {
+    const passwordRegex =
+      /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=]).{6,}$/;
+    return passwordRegex.test(password);
+  };
+
+  const handleSubmit = async () => {
+    const isFirstnameValid = validateField(user.firstname);
+    const isLastnameValid = validateField(user.lastname);
+    const isUsernameValid = validateField(user.username);
+    const isCountryValid = validateField(user.country);
+    const isEmailValid = validateField(user.email);
+    const isPhoneValid = validateField(user.phone);
+    const isCityValid = validateField(user.city);
+    const isStateValid = validateField(user.state);
+    const isHouseValid = validateField(user.house);
+    const isPasswordValid = validatePassword(user.password);
+    const isConfirmPasswordValid = validatePassword(user.confirmPassword);
+
+    setError({
+      firstname: !isFirstnameValid ? "Firstname is required" : "",
+      lastname: !isLastnameValid ? "Lastname is required" : "",
+      username: !isUsernameValid ? "Username is required" : "",
+      country: !isCountryValid ? "Country is required" : "",
+      email: !isEmailValid ? "Email is required" : "",
+      phone: !isPhoneValid ? "Phone number is required" : "",
+      city: !isCityValid ? "City is required" : "",
+      state: !isStateValid ? "State is required" : "",
+      house: !isHouseValid ? "House number is required" : "",
+      password: !isPasswordValid ? "Password must be at least 6 character with at least 1 lowercase, 1 uppercase, 1 digit and 1 special case" : "",
+      confirmPassword: !isConfirmPasswordValid ? "passwords do not match" : "",
+    });
+    setTimeout(() => {
+      setError({
+        firstname: "",
+        lastname: "",
+        username: "",
+        country: "",
+        email: "",
+        phone: "",
+        city: "",
+        state: "",
+        house: "",
+        password: "",
+        confirmPassword: "",
+      });
+    }, 2000);
+
+    if (user.password !== user.confirmPassword) {
+      setError({ ...error, confirmPassword: "passwords do not match" });
+    }
+  };
   return (
     <AuthContainer>
       <h2 className="text-[#4169e2] font-bold text-[34px]">
         Input account details
       </h2>
-
       <div className="space-y-6">
         <div className="flex justify-between mt-10 w-[100%]">
           <div className="w-[50%]">
-            <InputField type="text" label="First name" />
+            <InputField
+              type="text"
+              label="First name"
+              value={user.firstname}
+              onChange={(e) => setUser({ ...user, firstname: e.target.value })}
+              firstNameError={error.firstname}
+            />
           </div>
           <div className="w-[45%]">
-            <InputField type="text" label="Last name" />
+            <InputField
+              type="text"
+              label="Last name"
+              value={user.lastname}
+              onChange={(e) => setUser({ ...user, lastname: e.target.value })}
+              lastNameError={error.lastname}
+            />
           </div>
         </div>
-        <InputField type="text" label="Username" />
+        <InputField
+          type="text"
+          label="Username"
+          value={user.username}
+          onChange={(e) => setUser({ ...user, username: e.target.value })}
+          userNameError={error.username}
+        />
         <div>
           <h2 className="font-semibold text-[18px] text-[#6c8073] pb-2">
             Country
           </h2>
           <select
             name="country"
+            value={user.country}
+            onChange={(e) => setUser({ ...user, country: e.target.value })}
             className="w-[100%] px-4 outline-none bg-[#fff] border border-[#333] py-3"
           >
             <option value="select">Select</option>
@@ -30,29 +141,73 @@ const Signup = () => {
           </select>
         </div>
 
-        <InputField type="email" label="Email" />
-        <InputField type="text" label="Phone Number" />
+        <InputField
+          type="email"
+          label="Email"
+          value={user.email}
+          onChange={(e) => setUser({ ...user, email: e.target.value })}
+          emailError={error.email}
+        />
+        <InputField
+          type="text"
+          label="Phone Number"
+          value={user.phone}
+          onChange={(e) => setUser({ ...user, phone: e.target.value })}
+          phoneError={error.phone}
+        />
         <div className="flex justify-between w-[100%]">
           <div className="w-[45%]">
-            <InputField type="text" label="City" />
+            <InputField
+              type="text"
+              label="City"
+              value={user.city}
+              onChange={(e) => setUser({ ...user, city: e.target.value })}
+              cityError={error.city}
+            />
           </div>
           <div className="w-[50%]">
             <h2 className="font-semibold text-[18px] text-[#6c8073] pb-2">
               Select State
             </h2>
             <select
-              name="country"
+              name="state"
+              value={user.state}
+              onChange={(e) => setUser({ ...user, state: e.target.value })}
               className="px-4 w-[100%] outline-none bg-[#fff] border border-[#333] py-2.5"
             >
               <option value="select">Select</option>
-              <option value="Nigeria(default)">Nigeria(default)</option>
+              {States.map((state) => (
+                <option key={state.id} value={state.state}>
+                  {state.state}
+                </option>
+              ))}
             </select>
           </div>
         </div>
 
-        <InputField type="text" label="House/APT/Flat Number" />
-        <InputField type="password" label="Password" />
-        <InputField type="password" label="Confirm Password" />
+        <InputField
+          type="text"
+          label="House/APT/Flat Number"
+          value={user.house}
+          onChange={(e) => setUser({ ...user, house: e.target.value })}
+          houseError={error.house}
+        />
+        <InputField
+          type="password"
+          label="Password"
+          value={user.password}
+          onChange={(e) => setUser({ ...user, password: e.target.value })}
+          passwordError={error.password}
+        />
+        <InputField
+          type="password"
+          label="Confirm Password"
+          value={user.confirmPassword}
+          onChange={(e) =>
+            setUser({ ...user, confirmPassword: e.target.value })
+          }
+          confirmPasswordError={error.confirmPassword}
+        />
       </div>
 
       <p className="text-[14px] text-[#333] pt-6">
@@ -61,7 +216,10 @@ const Signup = () => {
         <span className="text-[#4169e2]">terms of service</span> and{" "}
         <span className="text-[#4169e2]">privacy policy</span>.
       </p>
-      <button className="text-[#fff] py-3 mt-4 w-[100%] flex justify-center bg-[#000]">
+      <button
+        onClick={handleSubmit}
+        className="text-[#fff] py-3 mt-4 w-[100%] flex justify-center bg-[#000]"
+      >
         Continue
       </button>
     </AuthContainer>
--- src/components/default/InputField.tsx
+++ src/components/default/InputField.tsx
@@ -1,17 +1,75 @@
-import { FC } from "react";
+import { FC, InputHTMLAttributes } from "react";
 
-interface InputProps {
-    label: string;
-    type: string;
+interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
+  label: string;
+  type: string;
+  firstNameError?: string;
+  lastNameError?: string;
+  userNameError?: string;
+  emailError?: string;
+  phoneError?: string;
+  cityError?: string;
+  houseError?: string;
+  passwordError?: string;
+  confirmPasswordError?: string;
 }
 
-const InputField: FC<InputProps> = ({ label, type }) => {
+const InputField: FC<InputProps> = ({
+  label,
+  type,
+  firstNameError,
+  lastNameError,
+  userNameError,
+  emailError,
+  phoneError,
+  cityError,
+  houseError,
+  passwordError,
+  confirmPasswordError,
+  ...props
+}) => {
   return (
     <div className="space-y-2">
-        <h2 className="font-semibold text-[18px] text-[#6c8073]">{label}</h2>
-        <input type={type} className="border border-[#333] outline-none py-2 px-4 w-[100%]" />
+      <h2 className="font-semibold text-[18px] text-[#6c8073]">{label}</h2>
+      <input
+        type={type}
+        className={
+          `
+          ${firstNameError && "border border-red-500"}
+          ${lastNameError && "border border-red-500"}
+          ${userNameError && "border border-red-500"}
+          ${emailError && "border border-red-500"}
+          ${phoneError && "border border-red-500"}
+          ${cityError && "border border-red-500"}
+          ${houseError && "border border-red-500"}
+          ${passwordError && "border border-red-500"}
+          ${confirmPasswordError && "border border-red-500"}
+          border border-[#333] outline-none py-2 px-4 w-[100%]
+          `
+        }
+        {...props}
+      />
+      {firstNameError && (
+        <p className="text-red-500 text-[12px]">{firstNameError}</p>
+      )}
+      {lastNameError && (
+        <p className="text-red-500 text-[12px]">{lastNameError}</p>
+      )}
+      {userNameError && (
+        <p className="text-red-500 text-[12px]">{userNameError}</p>
+      )}
+      {emailError && <p className="text-red-500 text-[12px]">{emailError}</p>}
+      {phoneError && <p className="text-red-500 text-[12px]">{phoneError}</p>}
+      {cityError && <p className="text-red-500 text-[12px]">{cityError}</p>}
+      {houseError && <p className="text-red-500 text-[12px]">{houseError}</p>}
+      {passwordError && (
+        <p className="text-red-500 text-[12px]">{passwordError}</p>
+      )}
+      {confirmPasswordError && (
+        <p className="text-red-500 text-[12px]">{confirmPasswordError}</p>
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default InputField
+export default InputField;
