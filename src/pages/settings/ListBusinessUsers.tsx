ab023e2cbaf97c26a819052592cd3c74ce7e21f1
Frokesy <akindeleayanfeoluwa@gmail.com>
2023-12-04 08:41:10 +0100

    needs some fixing: encountered cors error

--- src/pages/settings/ListBusinessUsers.tsx
+++ src/pages/settings/ListBusinessUsers.tsx
@@ -2,23 +2,62 @@ import { FaTrashAlt } from "react-icons/fa";
 import MainContainer from "../../components/containers/MainContainer";
 import { NavLink } from "react-router-dom";
 import { useEffect, useState } from "react";
+import { toast, ToastContainer } from "react-toastify";
 
 type Users = {
-    _id: string;
-    access_tier: string;
-    business: string;
-    createdAt: string;
-    email: string;
-    firstname: string;
-    lastname: string;
-    org_name: string;
-    updatedAt: string;
-}
+  _id: string;
+  access_tier: string;
+  business: string;
+  createdAt: string;
+  email: string;
+  firstname: string;
+  lastname: string;
+  org_name: string;
+  updatedAt: string;
+};
 
 const ListBusinessUsers = () => {
   const [users, setUsers] = useState<Users[]>([]);
   const access_token =
     "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6InJhaG1sYWRhcmFtaWRlQGdtYWlsLmNvbSIsInVzZXJJZCI6IjY1NjhmODI5YmY4NTc3MDAzZTVhYmMzZiIsInVzZXJuYW1lIjoiUmFobWxhZCBBcmFtaWRlIiwiZmlyc3RuYW1lIjoiNjU2OGY4MjhiZjg1NzcwMDNlNWFiYzNkIiwidHlwZSI6IkFDQ0VTU19UT0tFTiIsImlhdCI6MTcwMTY3MjA5OSwiZXhwIjoxNzAxNjc1Njk5fQ.H5e1daGwjfWvliIzoDQv_XuNxPNJCSdUOB9EjEaPZso";
+
+  const deleteUser = (id: string) => {
+    fetch(
+      `https://qcbackend.onrender.com/api/v1/business_admin/user/delete/?id=${id}`,
+      {
+        method: "DELETE",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${access_token}`,
+        },
+      }
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        if (data.status === "success") {
+          toast.success("User deleted successfully", {
+            position: "top-center",
+            autoClose: 500,
+            hideProgressBar: true,
+            closeOnClick: true,
+            pauseOnHover: false,
+            draggable: true,
+            progress: undefined,
+          });
+          window.location.reload();
+        } else {
+          toast.error("Something went wrong", {
+            position: "top-center",
+            autoClose: 500,
+            hideProgressBar: true,
+            closeOnClick: true,
+            pauseOnHover: false,
+            draggable: true,
+            progress: undefined,
+          });
+        }
+      });
+  };
 
   const backToSettings = () => {
     window.history.back();
@@ -42,6 +81,7 @@ const ListBusinessUsers = () => {
   }, []);
   return (
     <MainContainer activeTab="Settings">
+      <ToastContainer />
       <span onClick={backToSettings} className="cursor-pointer">
         {"<< "}back
       </span>
@@ -71,19 +111,27 @@ const ListBusinessUsers = () => {
           </thead>
           <tbody className="text-[#333]">
             {users.map((user) => (
-                <tr key={user._id} className="border-b border-[#ccc] hover:bg-[#f1f1f1]">
+              <tr
+                key={user._id}
+                className="border-b border-[#ccc] hover:bg-[#f1f1f1]"
+              >
                 <td className="py-3 px-4 text-center">{user.firstname}</td>
                 <td className="py-3 px-4 text-center">{user.lastname}</td>
                 <td className="py-3 px-4 text-center">{user.email}</td>
                 <td className="py-3 px-4 text-center">{user.org_name}</td>
                 <td className="py-3 px-4 text-center">{user.access_tier}</td>
-                <td className="py-3 px-4 text-center">{new Date(user.createdAt).toLocaleDateString()}</td>
-                <td className="py-3 px-4 text-center text-[#ee2020] flex items-center justify-center space-x-1">
+                <td className="py-3 px-4 text-center">
+                  {new Date(user.createdAt).toLocaleDateString()}
+                </td>
+                <td
+                  onClick={() => deleteUser(user._id)}
+                  className="py-3 px-4 text-center cursor-pointer text-[#ee2020] flex items-center justify-center space-x-1"
+                >
                   <FaTrashAlt />
                   <span>delete user</span>
                 </td>
               </tr>
-              ))}
+            ))}
           </tbody>
         </table>
       </div>
--- src/pages/settings/NewUser.tsx
+++ src/pages/settings/NewUser.tsx
@@ -1,52 +1,120 @@
+import { useState } from "react";
 import MainContainer from "../../components/containers/MainContainer";
+import { toast, ToastContainer } from "react-toastify";
 
 const NewUser = () => {
+  const [user, setUser] = useState({
+    firstname: "",
+    lastname: "",
+    email: "",
+    password: "",
+  });
   const backToSettings = () => {
     window.history.back();
   };
+
+  const access_token =
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6InJhaG1sYWRhcmFtaWRlQGdtYWlsLmNvbSIsInVzZXJJZCI6IjY1NjhmODI5YmY4NTc3MDAzZTVhYmMzZiIsInVzZXJuYW1lIjoiUmFobWxhZCBBcmFtaWRlIiwiZmlyc3RuYW1lIjoiNjU2OGY4MjhiZjg1NzcwMDNlNWFiYzNkIiwidHlwZSI6IkFDQ0VTU19UT0tFTiIsImlhdCI6MTcwMTY3MjA5OSwiZXhwIjoxNzAxNjc1Njk5fQ.H5e1daGwjfWvliIzoDQv_XuNxPNJCSdUOB9EjEaPZso";
+
+  const createUser = () => {
+    fetch(
+      "https://qcbackendtest.onrender.com/api/v1/business_admin/user/create/",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${access_token}`,
+        },
+        body: JSON.stringify(user),
+      }
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        if (data.status === "success") {
+          toast.success("User created successfully", {
+            position: "top-center",
+            autoClose: 500,
+            hideProgressBar: true,
+            closeOnClick: true,
+            pauseOnHover: false,
+            draggable: true,
+            progress: undefined,
+          });
+          window.location.reload();
+        } else {
+          toast.error("Something went wrong", {
+            position: "top-center",
+            autoClose: 500,
+            hideProgressBar: true,
+            closeOnClick: true,
+            pauseOnHover: false,
+            draggable: true,
+            progress: undefined,
+          });
+        }
+      });
+  };
+
   return (
     <MainContainer activeTab="Settings">
-      <span onClick={backToSettings} className="cursor-pointer">
-        {"<< "}back
-      </span>
+        <ToastContainer />
+        <span onClick={backToSettings} className="cursor-pointer">
+            {"<< "}back
+        </span>
 
-      <h2 className="text-[32px] font-semibold mt-3">Create New User</h2>
-      <div className="mt-10 w-[50%] space-y-8">
+        <h2 className="text-[32px] font-semibold mt-3">Create New User</h2>
+        <div className="mt-10 w-[50%] space-y-8">
         <div className="space-y-2">
-            <label className="text-[#333] px-1 font-semibold">Firstname</label>
-            <input
-                type="text"
-                placeholder="Enter firstname"
-                className="bg-[#f1f1f1] w-[100%] py-2 px-4 rounded-lg outline-none border-none"
-            />
+          <label className="text-[#333] px-1 font-semibold">Firstname</label>
+          <input
+            type="text"
+            required
+            value={user.firstname}
+            onChange={(e) => setUser({ ...user, firstname: e.target.value })}
+            placeholder="Enter firstname"
+            className="bg-[#f1f1f1] w-[100%] py-2 px-4 rounded-lg outline-none border-none"
+          />
         </div>
         <div className="space-y-2">
-            <label className="text-[#333] px-1 font-semibold">Lastname</label>
-            <input
-                type="text"
-                placeholder="Enter lastname"
-                className="bg-[#f1f1f1] w-[100%] py-2 px-4 rounded-lg outline-none border-none"
-            />
+          <label className="text-[#333] px-1 font-semibold">Lastname</label>
+          <input
+            type="text"
+            required
+            value={user.lastname}
+            onChange={(e) => setUser({ ...user, lastname: e.target.value })}
+            placeholder="Enter lastname"
+            className="bg-[#f1f1f1] w-[100%] py-2 px-4 rounded-lg outline-none border-none"
+          />
         </div>
         <div className="space-y-2">
-            <label className="text-[#333] px-1 font-semibold">Email</label>
-            <input
-                type="text"
-                placeholder="Enter email"
-                className="bg-[#f1f1f1] w-[100%] py-2 px-4 rounded-lg outline-none border-none"
-            />
+          <label className="text-[#333] px-1 font-semibold">Email</label>
+          <input
+            type="text"
+            required
+            value={user.email}
+            onChange={(e) => setUser({ ...user, email: e.target.value })}
+            placeholder="Enter email"
+            className="bg-[#f1f1f1] w-[100%] py-2 px-4 rounded-lg outline-none border-none"
+          />
         </div>
         <div className="space-y-2">
-            <label className="text-[#333] px-1 font-semibold">Password</label>
-            <input
-                type="password"
-                placeholder="Enter password"
-                className="bg-[#f1f1f1] w-[100%] py-2 px-4 rounded-lg outline-none border-none"
-            />
+          <label className="text-[#333] px-1 font-semibold">Password</label>
+          <input
+            type="password"
+            required
+            value={user.password}
+            onChange={(e) => setUser({ ...user, password: e.target.value })}
+            placeholder="Enter password"
+            className="bg-[#f1f1f1] w-[100%] py-2 px-4 rounded-lg outline-none border-none"
+          />
         </div>
         <div className="flex justify-end mt-6">
-          <button className="bg-[#4169e2] py-2 px-4 text-[#fff] font-semibold rounded-lg">
-             Submit
+          <button
+            onClick={createUser}
+            className="bg-[#4169e2] py-2 px-4 text-[#fff] font-semibold rounded-lg"
+          >
+            Submit
           </button>
         </div>
       </div>
