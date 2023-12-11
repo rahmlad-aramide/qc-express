33aaab37329f11e2847390690e47e05822db9fbe
Frokesy <akindeleayanfeoluwa@gmail.com>
2023-12-11 07:30:38 +0100

    added some extra handling

--- src/pages/feedback/index.tsx
+++ src/pages/feedback/index.tsx
@@ -2,6 +2,8 @@ import { useEffect, useState } from "react";
 import { MainContainer } from "../../components";
 import { ToastContainer, toast } from "react-toastify";
 import Modal from "../../components/default/Modal";
+import Skeleton from "react-loading-skeleton";
+import "react-loading-skeleton/dist/skeleton.css";
 
 type Feedbacks = {
   _id: string;
@@ -19,6 +21,7 @@ type Feedbacks = {
 const Feedback = () => {
   const [feedbacks, setFeedbacks] = useState<Feedbacks[]>([]);
   const [showModal, setShowModal] = useState(false);
+  const [loading, setLoading] = useState(true);
 
   const closeModal = () => setShowModal(false);
 
@@ -77,6 +80,7 @@ const Feedback = () => {
       .then((res) => res.json())
       .then((data) => {
         setFeedbacks(data.data.docs);
+        setLoading(false);
       });
   }, [access_token]);
 
@@ -84,7 +88,7 @@ const Feedback = () => {
     <MainContainer activeTab="Feedback">
       <ToastContainer />
       <div className="mt-10">
-        <div className="flex justify-between items-center">
+        <div className="flex justify-between items-center w-[90%]">
           <h2 className="text-[32px] font-semibold mt-3">Feedbacks</h2>
           <div className="flex text-[14px] space-x-6">
             <span
@@ -95,43 +99,59 @@ const Feedback = () => {
             </span>
           </div>
         </div>
-        <div className="mt-10 w-[100%]">
-          <table className="w-[100%]">
-            <thead>
-              <tr className="border-b border-[#ccc] uppercase text-[15px] text-[#6c8073]">
-                <th className="font-bold text-center">Admin details</th>
-                <th className="font-bold text-center">Message</th>
-                <th className="font-bold">Action</th>
-              </tr>
-            </thead>
+        <div className="mt-10 w-[90%]">
+          {loading ? (
+            <Skeleton count={5} />
+          ) : (
+            <div>
+              {feedbacks.length > 0 ? (
+                <table className="w-[100%]">
+                  <thead className="">
+                    <tr className="border border-[#ccc] uppercase text-[15px] text-[#6c8073] bg-[#f1f1f1]">
+                      <th className="font-bold text-left px-4">
+                        Admin details
+                      </th>
+                      <th className="font-bold text-left">Message</th>
+                      <th className="font-bold text-right px-4">Action</th>
+                    </tr>
+                  </thead>
 
-            <tbody className="text-[#333]">
-              {feedbacks.map((feedback) => (
-                <tr
-                  key={feedback._id}
-                  className="border-b border-[#ccc] hover:bg-[#f1f1f1]"
-                >
-                  <td className="py-3">
-                    <div className="flex flex-col space-x-2 text-center">
-                      <p className="font-semibold">{feedback.admin_name}</p>
-                      <p className="text-[12px]">{feedback.email}</p>
-                    </div>
-                  </td>
-                  <td className="py-3 px-4 text-center">
-                    <p className="">{feedback.content}</p>
-                  </td>
-                  <td className="py-3 px-4 text-center">
-                    <p
-                      className="text-[14px] text-[#ee2020] font-semibold cursor-pointer"
-                      onClick={() => deleteFeedback(feedback._id)}
-                    >
-                      delete feedback
-                    </p>
-                  </td>
-                </tr>
-              ))}
-            </tbody>
-          </table>
+                  <tbody className="text-[#333]">
+                    {feedbacks.map((feedback) => (
+                      <tr
+                        key={feedback._id}
+                        className="border border-[#ccc] hover:bg-[#f1f1f1]"
+                      >
+                        <td className="py-3 px-4">
+                          <div className="flex flex-col text-left">
+                            <p className="font-semibold">
+                              {feedback.admin_name}
+                            </p>
+                            <p className="text-[12px]">{feedback.email}</p>
+                          </div>
+                        </td>
+                        <td className="py-3 text-left">
+                          <p className="">{feedback.content}</p>
+                        </td>
+                        <td className="py-3 text-right pr-4">
+                          <p
+                            className="text-[14px] text-[#ee2020] font-semibold cursor-pointer"
+                            onClick={() => deleteFeedback(feedback._id)}
+                          >
+                            delete feedback
+                          </p>
+                        </td>
+                      </tr>
+                    ))}
+                  </tbody>
+                </table>
+              ) : (
+                <p className="text-[#333] text-[15px] text-center">
+                  No feedbacks found. Please add a new feedback
+                </p>
+              )}
+            </div>
+          )}
         </div>
         {showModal && <Modal closeModal={closeModal} />}
       </div>
--- src/pages/settings/ListBusinessUsers.tsx
+++ src/pages/settings/ListBusinessUsers.tsx
@@ -4,6 +4,8 @@ import { NavLink } from "react-router-dom";
 import { useEffect, useState } from "react";
 import { toast, ToastContainer } from "react-toastify";
 import { IoChevronBackOutline } from "react-icons/io5";
+import Skeleton from "react-loading-skeleton";
+import "react-loading-skeleton/dist/skeleton.css";
 
 type Users = {
   _id: string;
@@ -19,6 +21,12 @@ type Users = {
 
 const ListBusinessUsers = () => {
   const [users, setUsers] = useState<Users[]>([]);
+  const [loading, setLoading] = useState(true);
+
+  const access_token = sessionStorage
+    .getItem("access_token")
+    ?.replace(/["']/g, "");
+
   const deleteUser = (id: string) => {
     fetch(
       `https://qcbackend.onrender.com/api/v1/business_admin/user/delete/?id=${id}`,
@@ -26,7 +34,7 @@ const ListBusinessUsers = () => {
         method: "DELETE",
         headers: {
           "Content-Type": "application/json",
-          Authorization: `Bearer ${sessionStorage.getItem("access_token")}`,
+          Authorization: `Bearer ${access_token}`,
         },
       }
     )
@@ -63,10 +71,6 @@ const ListBusinessUsers = () => {
     window.history.back();
   };
 
-  const access_token = sessionStorage
-    .getItem("access_token")
-    ?.replace(/["']/g, "");
-
   useEffect(() => {
     fetch(
       "https://qcbackend.onrender.com/api/v1/business_admin/user/fetch?tier=2&page=1&limit=10",
@@ -80,6 +84,7 @@ const ListBusinessUsers = () => {
     )
       .then((res) => res.json())
       .then((data) => {
+        setLoading(false);
         setUsers(data.data.docs);
       });
   }, [access_token]);
@@ -87,12 +92,12 @@ const ListBusinessUsers = () => {
     <MainContainer activeTab="Settings">
       <ToastContainer />
       <button
-          onClick={backToSettings}
-          className="cursor-pointer flex items-center text-sm font-semibold"
-        >
-          <IoChevronBackOutline size={18} className="mr-1" />
-          Back
-        </button>
+        onClick={backToSettings}
+        className="cursor-pointer flex items-center text-sm font-semibold"
+      >
+        <IoChevronBackOutline size={18} className="mr-1" />
+        Back
+      </button>
 
       <div className="flex justify-between w-[100%] mt-2">
         <h2 className="text-lg font-semibold">Business Users</h2>
@@ -105,43 +110,59 @@ const ListBusinessUsers = () => {
         </div>
       </div>
       <div className="mt-10 w-[100%]">
-        <table className="border-collapse border border-[#ccc] w-full">
-          <thead>
-            <tr className="border-b border-[#ccc]">
-              <th className="font-semibold py-1">Firstname</th>
-              <th className="font-semibold">Lastname</th>
-              <th className="font-semibold">Email</th>
-              <th className="font-semibold">Organization Name</th>
-              <th className="font-semibold">AccessTier</th>
-              <th className="font-semibold">Date Joined</th>
-              <th className="font-semibold">Action</th>
-            </tr>
-          </thead>
-          <tbody className="text-[#333]">
-            {users.map((user) => (
-              <tr
-                key={user._id}
-                className="border-b border-[#ccc] hover:bg-[#f1f1f1]"
-              >
-                <td className="py-3 px-4 text-center">{user.firstname}</td>
-                <td className="py-3 px-4 text-center">{user.lastname}</td>
-                <td className="py-3 px-4 text-center">{user.email}</td>
-                <td className="py-3 px-4 text-center">{user.org_name}</td>
-                <td className="py-3 px-4 text-center">{user.access_tier}</td>
-                <td className="py-3 px-4 text-center">
-                  {new Date(user.createdAt).toLocaleDateString()}
-                </td>
-                <td
-                  onClick={() => deleteUser(user._id)}
-                  className="py-3 px-4 text-center cursor-pointer text-[#ee2020] flex items-center justify-center space-x-1"
-                >
-                  <FaTrashAlt />
-                  <span>delete user</span>
-                </td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
+        {loading ? (
+          <Skeleton count={5} />
+        ) : (
+          <div>
+            {users.length > 0 ? (
+              <table className="border-collapse border border-[#ccc] w-full">
+                <thead>
+                  <tr className="border-b border-[#ccc]">
+                    <th className="font-semibold py-1">Firstname</th>
+                    <th className="font-semibold">Lastname</th>
+                    <th className="font-semibold">Email</th>
+                    <th className="font-semibold">Organization Name</th>
+                    <th className="font-semibold">AccessTier</th>
+                    <th className="font-semibold">Date Joined</th>
+                    <th className="font-semibold">Action</th>
+                  </tr>
+                </thead>
+                <tbody className="text-[#333]">
+                  {users.map((user) => (
+                    <tr
+                      key={user._id}
+                      className="border-b border-[#ccc] hover:bg-[#f1f1f1]"
+                    >
+                      <td className="py-3 px-4 text-center">
+                        {user.firstname}
+                      </td>
+                      <td className="py-3 px-4 text-center">{user.lastname}</td>
+                      <td className="py-3 px-4 text-center">{user.email}</td>
+                      <td className="py-3 px-4 text-center">{user.org_name}</td>
+                      <td className="py-3 px-4 text-center">
+                        {user.access_tier}
+                      </td>
+                      <td className="py-3 px-4 text-center">
+                        {new Date(user.createdAt).toLocaleDateString()}
+                      </td>
+                      <td
+                        onClick={() => deleteUser(user._id)}
+                        className="py-3 px-4 text-center cursor-pointer text-[#ee2020] flex items-center justify-center space-x-1"
+                      >
+                        <FaTrashAlt />
+                        <span>delete user</span>
+                      </td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+            ) : (
+              <p className="text-[#333] text-[15px] text-center">
+                No users found
+              </p>
+            )}
+          </div>
+        )}
       </div>
     </MainContainer>
   );
