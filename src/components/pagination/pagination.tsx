f1a0cd453824ab4636d4707dbc68a1b2c432687a
rahmlad-aramide <abdrahmanoladimeji02@gmail.com>
2023-12-16 09:02:50 +0100

    fix: fixed lint error

--- src/components/pagination/pagination.tsx
+++ src/components/pagination/pagination.tsx
@@ -1,4 +1,4 @@
-/* eslint-disable react-refresh/only-export-components */
+
 import React from "react";
 import { usePagination, DOTS } from "../../custom-hooks/usePagination";
 import { IoIosArrowBack, IoIosArrowForward } from "react-icons/io";
